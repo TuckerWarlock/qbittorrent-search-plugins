2a05c13b18210bc771e80c6e6cb33cd85b325016
Tucker Warlock <59034283+TuckerWarlock@users.noreply.github.com>
2023-08-26 22:59:53 -0600

    fix magnet link for bt4gprx.py

--- bt4gprx.py
+++ bt4gprx.py
@@ -3,19 +3,23 @@
 
 from html.parser import HTMLParser
 from urllib.parse import urljoin
-from helpers import retrieve_url
+from helpers import retrieve_url, download_file
 from novaprinter import prettyPrinter
+import re
 import json
 
+
 class bt4gprx(object):
-    
     url = "https://bt4gprx.com/"
     name = "bt4gprx"
     supported_categories = {'all': '', 'movies': 'movie/', 'tv': 'movie/', 'music': 'audio/', 'books': 'doc/', 'software': 'app/'}
 
+    
+    def __init__(self):
+        self.trackerlist = []
 
+    
     class MyHTMLParser(HTMLParser):
-
         def __init__(self):
             super().__init__()
             self.is_in_container = False
@@ -25,11 +29,12 @@ class bt4gprx(object):
             self.temp_result = {}
             self.results = []
 
+        
         def feed(self, feed: str) -> None:
             super().feed(feed)
             return self.results
 
-
+        
         def handle_starttag(self, tag, attrs):
             attr_dict = {x[0]:x[1] for x in attrs}
             if tag == "div":
@@ -45,10 +50,12 @@ class bt4gprx(object):
                     idname = attr_dict.get("id", "")
                     self.b_value = "filesize" if "cpill" in classname else idname
 
+        
         def handle_endtag(self, tag):        
             if tag == "div":
                 self.is_in_entry = False
 
+        
         def handle_data(self, data):
             if self.b_value != "":
                 self.temp_result[self.b_value] = data
@@ -57,6 +64,7 @@ class bt4gprx(object):
                     self.temp_result = {}
                 self.b_value = ""
 
+    
     def search(self, term, cat="all"):
         pagenumber = 1
         while pagenumber <= 10:
@@ -74,23 +82,40 @@ class bt4gprx(object):
         except Exception as e:
             return []
 
+    
     def download_torrent(self, info):
-        if "trackerlist" not in self.__dict__:
+        try:
+            content = retrieve_url(info)
+            match = re.search(r'href="//(downloadtorrentfile.com/hash/[^"]+)', content)
+            if not match:
+                print("Failed to find downloadtorrentfile.com link.")
+                return
+            actual_link = "https:" + match.group(0)
+        except Exception as e:
+            print(f"Error extracting downloadtorrentfile.com link: {e}")
+            return
+        try:
+            hash_value = actual_link.split("/hash/")[1].split("?")[0]
+            name_value = actual_link.split("?name=")[1]
+        except Exception as e:
+            print(f"Error extracting hash and name: {e}")
+            return
+        if not self.trackerlist:
             self.trackerlist = json.loads(retrieve_url("https://downloadtorrentfile.com/trackerlist"))
-        magnet = f"magnet:?xt=urn:btih:{info.split('/')[-1]}&tr=" + "&tr=".join(self.trackerlist)
-        print(magnet + ' ' + info)
+        magnet = f"magnet:?xt=urn:btih:{hash_value}&dn={name_value}&tr=" + "&tr=".join(self.trackerlist)
+        return magnet
 
+    
     def pretty_print_results(self, results):
         for result in results:
+            magnet_link = self.download_torrent(urljoin(self.url, result['href']))
+            
             temp_result = {
                 'name': result['title'],
                 'size': result['filesize'],
                 'seeds': result['seeders'],
                 'leech': result['leechers'],
                 'engine_url': self.url,
-                'link': urljoin(self.url, result['href'])
+                'link': magnet_link if magnet_link else urljoin(self.url, result['href'])
             }
             prettyPrinter(temp_result)
-
-if __name__ == "__main__":
-    bt4gprx().search("how", cat="books")
